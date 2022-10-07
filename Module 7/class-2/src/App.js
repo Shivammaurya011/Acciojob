59cdff353d3e8502dd1ea804287afcec297f5167
Shivammaurya011 <shivammaurya011@gmail.com>
2022-10-07 18:45:36 +0530

    06/10/2022

---
+++ Module 7/class-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Component from './components/Component';
+
+function App() {
+  return (
+    <div style={{background:'aqua', color:'red'}}>
+      Shivam Maurya
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Module 7/class-2/src/components/Component.js
@@ -0,0 +1,4 @@
+function Component(){
+    return <div>This is example of Component</div>
+}
+export default Component;
